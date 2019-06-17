ea4bfd3df4de9c585312380974ef03c352079495
Jay Maas <jaymaas.dev@gmail.com>
2019-06-17 14:40:31 -0600

    Repair additional
    3 more tests added to repair.
    
    Repair is completed

--- enhancing/enhancer.test.js
+++ enhancing/enhancer.test.js
@@ -4,6 +4,9 @@ describe('enhancer.js', () => {
     describe('repair()', () => {
         it('restores durability to 100', () => {
             expect(repair({ durability: 80 }).durability).toBe(100)
+            expect(repair({ durability: -80 }).durability).toBe(100)
+            expect(repair({ durability: 0 }).durability).toBe(100)
+            expect(repair({ durability: 100 }).durability).toBe(100)
         })
     })
 })
